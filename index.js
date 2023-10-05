dcb7c009612486e07ecf37f3e767837578472cc5
hou00127 <hou00127@umn.edu>
2023-10-05 19:25:00 -0400

    Oct. 3

---
+++ index.js
@@ -0,0 +1,8 @@
+// Get a reference to the div element by its ID
+var clickableDiv = document.getElementById("ruizhang");
+
+// Add a click event listener to the div
+clickableDiv.addEventListener("click", function() {
+    // Redirect to a URL when the div is clicked
+    window.open("http://Ruizhang.umn.edu", "_blank"); // Replace with your desired URL
+});
